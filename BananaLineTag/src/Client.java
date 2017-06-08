ff53822f4f35d66654b767b44bac5513d3bff3e3
leif <leifgg42@gmail.com>
2017-06-07 20:12:31 -0700

    Added Server and Client classes.

---
+++ BananaLineTag/src/Client.java
@@ -0,0 +1,12 @@
+
+public class Client extends Thread {
+	
+	public static void main(String[] args) {
+	
+	}
+	
+	public void run() {
+		
+	}
+	
+}
---
+++ BananaLineTag/src/Server.java
@@ -0,0 +1,12 @@
+
+public class Server extends Thread {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+}
