e177565163cb5cd0d12f5b30a3bd8e6d447bf03e
leif <leifgg42@gmail.com>
2017-06-10 16:39:42 -0700

    Added a generate icon method to Map, and added imageToBytes and imageFromBytes to Utils.

--- BananaLineTag/src/Map.java
+++ BananaLineTag/src/Map.java
@@ -1,10 +1,44 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 
 public class Map implements Serializable {
 	
 	private static final long serialVersionUID = -5245572493686594653L;
 
+	public static BufferedImage generateIcon(Map map, int size) {
+		BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(map.r, map.g, map.b));
+		g.fillRect(0, 0, size, size);
+		g.setColor(Color.black);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		Stroke s = g2.getStroke();
+		
+		for (Line l : map.lines) {
+			int cap = l.round ? BasicStroke.CAP_ROUND : BasicStroke.CAP_SQUARE;
+			g2.setStroke(new BasicStroke(Math.max((int) ((size + size) / 2 * l.thickness), 1), cap, BasicStroke.JOIN_ROUND));
+			g.setColor(new Color(l.r, l.g, l.b, l.a));
+			g.drawLine((int) (size * l.x1), (int) (size * l.y1), (int) (size * l.x2), (int) (size * l.y2));
+		}
+		g2.setStroke(s);
+		return image;
+	}
 	
 	ArrayList<Line> lines = new ArrayList<Line>();
 	
