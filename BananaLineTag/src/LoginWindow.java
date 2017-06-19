1d64d509719beaad3b59629d47da755ae93fd832
leif <leifgg42@gmail.com>
2017-06-19 09:58:47 -0700

    Changed max name length to 10

--- BananaLineTag/src/LoginWindow.java
+++ BananaLineTag/src/LoginWindow.java
@@ -35,7 +35,7 @@ public class LoginWindow extends JFrame {
 	RestrictedField port = new RestrictedField(10, new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, 5);
 	JLabel connectMassage = new JLabel();
 	
-	JTextField username = new RestrictedField(10, new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-'}, 16);
+	JTextField username = new RestrictedField(10, new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-'}, 10);
 	JButton login = new JButton("Login");
 	JLabel loginMessage = new JLabel();
 	
